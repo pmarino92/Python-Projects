61d61b3ebd44e7382404419a2b7ebbe4bd3d62d9
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-08-23 14:21:36 -0700

    Django University Update

--- Classes/classApp/admin.py
+++ Classes/classApp/admin.py
@@ -4,3 +4,6 @@ from .models import djangoClasses
 
 # Register your models here.
 admin.site.register(djangoClasses)
+admin.site.register(Class1)
+admin.site.register(Class2)
+admin.site.register(Class3)
--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class djangoClasses (models.Model):
     title = models.CharField(max_length=30)
-    number = models.CharField(max_length=10)
+    number = models.IntegerField(max_length=10)
     name = models.CharField(max_length=30, default="", blank=True, null=False)
     duration = models.DecimalField(default=0.00, max_digits=10000, decimal_places=3)
 
@@ -11,3 +11,9 @@ class djangoClasses (models.Model):
 
     def __str__(self):
         return self.name
+
+
+#objects of class "djangoClasses"
+Class1 = djangoClasses('Biology', 101, 'Bill Cyrus', 1.5)
+Class2 = djangoClasses('History', 100, 'Lilly Covfefe', 1)
+Class3 = djangoClasses('Calculus', 110, 'Stephanie Gaby', 120)
