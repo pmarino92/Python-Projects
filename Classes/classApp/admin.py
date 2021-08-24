6363524955fa94f3e133a58951b52d70c1d6e8a7
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-08-24 09:25:38 -0700

    Django U 4

--- Classes/classApp/admin.py
+++ Classes/classApp/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
-from .models import djangoClasses, Biology, History, Calculus
+from .models import djangoClasses
 
 
 # Register your models here.
 admin.site.register(djangoClasses)
-admin.site.register(Biology)
-admin.site.register(History)
-admin.site.register(Calculus)
